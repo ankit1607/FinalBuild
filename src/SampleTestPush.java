7bed394c4537cb58f4a34ef26fb718ecab0fa067
Mohit <mohit110891@gmail.com>
2015-07-01 22:54:48 +0530

    src changed

--- src/SampleTestPush.java
+++ src/SampleTestPush.java
@@ -1,13 +1,18 @@
-
-
-
-public class SampleTestPush 
-{
+public class SampleTestPush {
+	
+	SampleTestPush simple;
+	
+	
 	public static void main(String[] args) 
 	{
 	  System.err.println("Hello!");	
 	  System.out.println(SampleTestPush.class);
 	  
 	  System.out.println("SampleTestPush"); 
+	  
+	  
+	}
+	public SampleTestPush(Object sim) {
+		this.simple = (SampleTestPush) sim;
 	}
 }
